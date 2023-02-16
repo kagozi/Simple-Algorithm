bc2520f071af23fb6b0aa691734672a44bdecb01
kagozi <alexkagozi.com>
2023-02-16 14:37:44 +0300

    Modified tests

--- README.md
+++ README.md
@@ -58,5 +58,4 @@ The value should be printed on the terminal after execution.
 assert max_in_row_min_in_col([[5, 16, 20], [9, 11, 18], [15, 16, 17]]) == [17]
 assert max_in_row_min_in_col([[100, 60, 20, 50], [70, 80, 10, 90], [10, 50, 44, 30]]) == [50]
 assert max_in_row_min_in_col([[5]]) == [5]
-assert max_in_row_min_in_col([[5], [10], [20]]) == [20]
 ```
--- simpleAlgorithm.py
+++ simpleAlgorithm.py
@@ -14,3 +14,11 @@ matrix = eval(matrix_str)
 
 result = max_in_row_min_in_col(matrix)
 print(result)
+
+
+# unit tests can be run to check validity of the code:
+assert max_in_row_min_in_col([[5, 16, 20], [9, 11, 18], [15, 16, 17]]) == [17]
+assert max_in_row_min_in_col(
+    [[100, 60, 20, 50], [70, 80, 10, 90], [10, 50, 44, 30]]) == [50]
+assert max_in_row_min_in_col([[5]]) == [5]
+
